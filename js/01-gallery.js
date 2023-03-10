800f63326fd5217c96eacf5f082f2154195fc5b3
biggujo <gen.style22@gmail.com>
2023-03-10 17:44:58 +0200

    Task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryListRef = document.querySelector(".gallery");
+let currentModalInstance = null; // * Current modal pointer
 
 // * Create gallery
 const galleryElements = createGalleryElements();
 galleryListRef.append(...galleryElements);
 
 // * Listen to image click
-
-galleryListRef.addEventListener("click", onGalleryClick);
+galleryListRef.addEventListener(
+  "click",
+  onGalleryClick,
+);
 
 function onGalleryClick(e) {
-
   e.preventDefault();
 
   if (!e.target.closest(".gallery__item")) {
@@ -22,13 +24,53 @@ function onGalleryClick(e) {
   const imageLink = e.target.parentNode.getAttribute("href");
   const imageAlt = e.target.getAttribute("alt");
 
-  const modal = basicLightbox.create(`
-    <div class="modal">
-        <img src="${imageLink}" alt="${imageAlt}">
+  const modal = basicLightbox.create(
+    `
+    <div> 
+      <img src="${imageLink}" alt="${imageAlt}">
     </div>
-`);
+    `,
+    {
+      onShow: onModalShow,
+      onClose: onModalClose,
+    },
+  );
 
   modal.show();
+}
+
+function onModalShow(instance) {
+  // * Save instance for further use
+  currentModalInstance = instance;
+
+  instance.element().addEventListener(
+    "click",
+    onModalInteraction,
+  );
+  document.addEventListener(
+    "keydown",
+    onModalInteraction,
+  );
+}
+
+function onModalClose(instance) {
+  instance.element().removeEventListener(
+    "click",
+    onModalInteraction,
+  );
+  document.removeEventListener(
+    "keydown",
+    onModalInteraction,
+  );
+}
+
+function onModalInteraction(e) {
+  // * Not LMB or the Escape button
+  if (e.code !== "Escape" && e.button !== 0) {
+    return;
+  }
+
+  currentModalInstance.close();
 }
 
 function createGalleryElements() {
@@ -46,21 +88,28 @@ function createGalleryElements() {
     item.classList.add("gallery__item");
 
     const link = document.createElement("a");
-    link.setAttribute("href", originalSrc);
+    link.classList.add("gallery__link");
+    link.setAttribute(
+      "href",
+      originalSrc,
+    );
 
     const img = document.createElement("img");
-    img.classList.add("gallery__image", "gallery__link");
-    img.setAttribute("src", previewSrc);
-    img.setAttribute("alt", description);
-
+    img.classList.add("gallery__image");
+    img.setAttribute(
+      "src",
+      previewSrc,
+    );
+    img.setAttribute(
+      "alt",
+      description,
+    );
+    img.dataset.source = originalSrc;
+    
     link.appendChild(img);
     item.appendChild(link);
     itemsElArray.push(item);
   }
 
   return itemsElArray;
-}
-
-function getImageSrc(e) {
-  return e.target.src;
 }
