e1c84f93ea383661e3fadc9136513309239e3044
biggujo <gen.style22@gmail.com>
2023-03-10 18:53:20 +0200

    Add task 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,10 +9,7 @@ const galleryElements = createGalleryElements();
 galleryListRef.append(...galleryElements);
 
 // * Listen to image click
-galleryListRef.addEventListener(
-  "click",
-  onGalleryClick,
-);
+galleryListRef.addEventListener("click", onGalleryClick);
 
 function onGalleryClick(e) {
   e.preventDefault();
@@ -24,17 +21,14 @@ function onGalleryClick(e) {
   const imageLink = e.target.parentNode.getAttribute("href");
   const imageAlt = e.target.getAttribute("alt");
 
-  const modal = basicLightbox.create(
-    `
+  const modal = basicLightbox.create(`
     <div> 
       <img src="${imageLink}" alt="${imageAlt}">
     </div>
-    `,
-    {
-      onShow: onModalShow,
-      onClose: onModalClose,
-    },
-  );
+    `, {
+    onShow: onModalShow,
+    onClose: onModalClose,
+  });
 
   modal.show();
 }
@@ -43,25 +37,13 @@ function onModalShow(instance) {
   // * Save instance for further use
   currentModalInstance = instance;
 
-  instance.element().addEventListener(
-    "click",
-    onModalInteraction,
-  );
-  document.addEventListener(
-    "keydown",
-    onModalInteraction,
-  );
+  instance.element().addEventListener("click", onModalInteraction);
+  document.addEventListener("keydown", onModalInteraction);
 }
 
 function onModalClose(instance) {
-  instance.element().removeEventListener(
-    "click",
-    onModalInteraction,
-  );
-  document.removeEventListener(
-    "keydown",
-    onModalInteraction,
-  );
+  instance.element().removeEventListener("click", onModalInteraction);
+  document.removeEventListener("keydown", onModalInteraction);
 }
 
 function onModalInteraction(e) {
@@ -89,23 +71,14 @@ function createGalleryElements() {
 
     const link = document.createElement("a");
     link.classList.add("gallery__link");
-    link.setAttribute(
-      "href",
-      originalSrc,
-    );
+    link.setAttribute("href", originalSrc);
 
     const img = document.createElement("img");
     img.classList.add("gallery__image");
-    img.setAttribute(
-      "src",
-      previewSrc,
-    );
-    img.setAttribute(
-      "alt",
-      description,
-    );
+    img.setAttribute("src", previewSrc);
+    img.setAttribute("alt", description);
     img.dataset.source = originalSrc;
-    
+
     link.appendChild(img);
     item.appendChild(link);
     itemsElArray.push(item);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListRef = document.querySelector(".gallery");
+
+// * Create gallery
+const galleryElements = createGalleryElements();
+galleryListRef.append(...galleryElements);
+
+function createGalleryElements() {
+  const itemsElArray = [];
+  const { length } = galleryItems;
+
+  for (let i = 0; i < length; i++) {
+    const {
+      preview: previewSrc,
+      original: originalSrc,
+      description,
+    } = galleryItems[i];
+
+    const item = document.createElement("a");
+    item.classList.add("gallery__item");
+    item.setAttribute("href", originalSrc);
+
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.setAttribute("src", previewSrc);
+    img.setAttribute("alt", description);
+
+    item.appendChild(img);
+    itemsElArray.push(item);
+  }
+
+  return itemsElArray;
+}
+
+new SimpleLightbox('.gallery a', {
+  animationSpeed: 150,
+  captionsData: "alt",
+  captionDelay: 250,
+  disableScroll: true,
+  scrollZoom: false, // ! false, as there are library-related errors:
+  // ! "simple-lightbox.min.js:1"
+  // ! "Unable to preventDefault inside passive event listener invocation."
+});
